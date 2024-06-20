c3e49e0533b9adc86fc1157cc002b48738a47bef
Akila_Lakshitha <akilalakshithaofficial@gmail.com>
2024-06-21 00:37:02 +0530

    Update

---
+++ App.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) {
+        //creating the frame
+        JFrame frame = new JFrame("Sorting Application");
+        
+    }
+}
